8d15db8ba22cb7ce863fc0fd52ef775625692870
Alexandre Dubé <adube@mapgears.com>
2014-03-03 11:57:30 -0500

    Simplyfy the cadus example
    This follows the 'Simplyfy the google-map example' change in
    commit caf59eea8f1299d7382bd85087134540065d8f45.

--- examples/cadus.js
+++ examples/cadus.js
@@ -13,9 +13,7 @@ goog.require('ol.style.Stroke');
 goog.require('ol.style.Style');
 
 
-var gmap = new google.maps.Map(document.getElementById('map'), {
-  center: new google.maps.LatLng(0, 0),
-  zoom: 1,
+var gmap = new google.maps.Map(document.getElementById('gmap'), {
   disableDefaultUI: true,
   keyboardShortcuts: false,
   draggable: false,
@@ -24,16 +22,17 @@ var gmap = new google.maps.Map(document.getElementById('map'), {
   streetViewControl: false
 });
 
-var olmap = document.createElement('div');
-olmap.style['width'] = '100%';
-olmap.style['height'] = '100%';
-gmap.controls[google.maps.ControlPosition.TOP_LEFT].push(olmap);
+var view = new ol.View2D();
+view.on('change:center', function() {
+  var center = ol.proj.transform(view.getCenter(), 'EPSG:3857', 'EPSG:4326');
+  gmap.setCenter(new google.maps.LatLng(center[1], center[0]));
+});
+view.on('change:resolution', function() {
+  gmap.setZoom(view.getZoom());
+});
 
-var olGeocoder;
-
-google.maps.event.addListenerOnce(gmap, 'idle', function() {
-
-  var vectorSource = new ol.source.GeoJSON(({
+var vector = new ol.layer.Vector({
+  source: new ol.source.GeoJSON(({
     object: {
       'type': 'FeatureCollection',
       'crs': {
@@ -44,50 +43,37 @@ google.maps.event.addListenerOnce(gmap, 'idle', function() {
       },
       'features': []
     }
-  }));
-
-  var dummy = new ol.layer.Vector({
-    source: vectorSource,
-    style: new ol.style.Style({
-      fill: new ol.style.Fill({
-        color: 'rgba(255, 255, 255, 0.6)'
-      }),
-      stroke: new ol.style.Stroke({
-        color: '#319FD3',
-        width: 1
-      })
+  })),
+  style: new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 255, 255, 0.6)'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#319FD3',
+      width: 1
     })
-  });
-
-  var center = gmap.getCenter();
-  var map = new ol.Map({
-    layers: [dummy],
-    interactions: ol.interaction.defaults({
-      altShiftDragRotate: false,
-      dragPan: false,
-      touchRotate: false
-    }).extend([new ol.interaction.DragPan({kinetic: false})]),
-    renderer: 'canvas',
-    target: olmap,
-    view: new ol.View2D({
-      center: ol.proj.transform([center.lng(), center.lat()],
-          'EPSG:4326', 'EPSG:3857'),
-      zoom: gmap.getZoom()
-    })
-  });
-
-  var view = map.getView().getView2D();
-  view.on('change:center', function() {
-    var center = ol.proj.transform(view.getCenter(),
-        'EPSG:3857', 'EPSG:4326');
-    gmap.setCenter(new google.maps.LatLng(center[1], center[0]));
-  });
-  view.on('change:resolution', function() {
-    gmap.setZoom(view.getZoom());
-  });
-
-  olGeocoder = new ol.control.GoogleMapsGeocoder({
-    'target': 'gmaps-geocoder'
-  });
-  map.addControl(olGeocoder);
+  })
 });
+
+var olMapDiv = document.getElementById('olmap');
+var map = new ol.Map({
+  layers: [vector],
+  interactions: ol.interaction.defaults({
+    altShiftDragRotate: false,
+    dragPan: false,
+    touchRotate: false
+  }).extend([new ol.interaction.DragPan({kinetic: false})]),
+  renderer: 'canvas',
+  target: olMapDiv,
+  view: view
+});
+view.setCenter([0, 0]);
+view.setZoom(1);
+
+olMapDiv.parentNode.removeChild(olMapDiv);
+gmap.controls[google.maps.ControlPosition.TOP_LEFT].push(olMapDiv);
+
+var olGeocoder = new ol.control.GoogleMapsGeocoder({
+  'target': 'gmaps-geocoder'
+});
+map.addControl(olGeocoder);
