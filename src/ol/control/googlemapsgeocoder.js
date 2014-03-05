ae5843eb5f47dcc4971d91d655eeb510e067f8df
Alexandre Dubé <adube@mapgears.com>
2014-03-05 09:26:51 -0500

    Add google.maps.Geocoder in control
    Now that GoogleMaps externs are included, a Geocoder could be added.
    When pressing 'enter' or clicking the button, the googlemapsgeocoder
    control now triggers the geocode method and populate the textbox
    accordingly.
    
    Work to do:
     * support multiple results
     * click on map to trigger geocode

--- src/ol/control/googlemapsgeocoder.js
+++ src/ol/control/googlemapsgeocoder.js
@@ -2,6 +2,10 @@ goog.provide('ol.control.GoogleMapsGeocoder');
 
 goog.require('goog.dom');
 goog.require('goog.dom.TagName');
+goog.require('goog.events');
+goog.require('goog.events.EventType');
+goog.require('goog.events.KeyCodes');
+goog.require('goog.string');
 goog.require('ol.control.Control');
 goog.require('ol.css');
 
@@ -35,10 +39,112 @@ ol.control.GoogleMapsGeocoder = function(opt_options) {
   goog.dom.appendChild(element, input);
   goog.dom.appendChild(element, button);
 
+  goog.events.listen(button, [
+    goog.events.EventType.TOUCHEND,
+    goog.events.EventType.CLICK
+  ], this.handleButtonPress_, false, this);
+
+  goog.events.listen(input, [
+    goog.events.EventType.KEYPRESS
+  ], this.handleInputKeypress_, false, this);
 
   goog.base(this, {
     element: element,
     target: options.target
   });
+
+  /**
+   * @private
+   * @type {Element}
+   */
+  this.input_ = input;
+
+  /**
+   * @private
+   * @type {google.maps.Geocoder}
+   */
+  this.geocoder_ = new google.maps.Geocoder();
+
 };
 goog.inherits(ol.control.GoogleMapsGeocoder, ol.control.Control);
+
+
+/**
+ * @param {goog.events.BrowserEvent} browserEvent Browser event.
+ * @private
+ */
+ol.control.GoogleMapsGeocoder.prototype.handleInputKeypress_ = function(
+    browserEvent) {
+
+  if (browserEvent.keyCode == goog.events.KeyCodes.ENTER) {
+    this.handleButtonPress_(browserEvent);
+  }
+};
+
+
+/**
+ * @param {goog.events.BrowserEvent} browserEvent Browser event.
+ * @private
+ */
+ol.control.GoogleMapsGeocoder.prototype.handleButtonPress_ = function(
+    browserEvent) {
+
+  browserEvent.preventDefault();
+
+  var input = this.input_;
+  var value = input.value;
+  if (!goog.string.isEmptySafe(value)) {
+    this.geocodeByAddress_(value);
+  }
+};
+
+
+/**
+ * @param {String} address The address to search
+ * @private
+ */
+ol.control.GoogleMapsGeocoder.prototype.geocodeByAddress_ = function(address) {
+
+  var geocoder = this.geocoder_;
+  var input = this.input_;
+
+  var formatted_address, x, y;
+  var result;
+  var tmpOutput = [];
+
+
+  geocoder.geocode(
+      {
+        'address': address
+      },
+      function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          // TODO: support multiple results
+          result = results[0];
+
+          formatted_address = result.formatted_address;
+          x = result.geometry.location.lng();
+          y = result.geometry.location.lat();
+
+          tmpOutput.push(formatted_address);
+          tmpOutput.push('\n');
+          tmpOutput.push('(');
+          tmpOutput.push(x);
+          tmpOutput.push(', ');
+          tmpOutput.push(y);
+          tmpOutput.push(')');
+
+          //alert(tmpOutput.join(''));
+
+          input.value = formatted_address;
+
+        } else {
+          // TODO: manage error message
+          alert(
+              'Geocode was not successful for the following reason: ' +
+              status
+          );
+        }
+      }
+  );
+};
