5b14e45702d126d4d88e4ba7c626e7fa86c2946a
Alexandre Dubé <adube@mapgears.com>
2014-03-10 11:31:22 -0400

    Add googlemapsdirections control and example

---
+++ src/ol/control/googlemapsdirections.js
@@ -0,0 +1,110 @@
+goog.provide('ol.control.GoogleMapsDirections');
+
+goog.require('goog.asserts');
+goog.require('goog.dom');
+goog.require('goog.dom.TagName');
+goog.require('ol.control.Control');
+goog.require('ol.control.GoogleMapsGeocoder');
+goog.require('ol.css');
+goog.require('ol.style.Style');
+
+
+
+/**
+ * Todo
+ * @constructor
+ * @extends {ol.control.Control}
+ * @param {olx.control.GoogleMapsDirectionsOptions=} opt_options Options.
+ */
+ol.control.GoogleMapsDirections = function(opt_options) {
+  var options = goog.isDef(opt_options) ? opt_options : {};
+
+  var className = 'ol-google-maps-directions';
+
+  var element = goog.dom.createDom(goog.dom.TagName.DIV, {
+    'class': className + ' ' + ol.css.CLASS_UNSELECTABLE
+  });
+
+  var startGeocoderElement = goog.dom.createDom(goog.dom.TagName.DIV, {
+    'class': 'ol-google-maps-directions-geocoder-start'
+  });
+  goog.dom.appendChild(element, startGeocoderElement);
+
+  var endGeocoderElement = goog.dom.createDom(goog.dom.TagName.DIV, {
+    'class': 'ol-google-maps-directions-geocoder-end'
+  });
+  goog.dom.appendChild(element, endGeocoderElement);
+
+
+  /**
+   * @type {Object}
+   * @private
+   */
+  this.geocoderComponentRestrictions_ = goog.isDef(
+      options.geocoderComponentRestrictions) ?
+      options.geocoderComponentRestrictions : {};
+
+  /**
+   * @type {ol.style.Style}
+   * @private
+   */
+  this.startIconStyle_ = options.startIconStyle;
+
+
+  /**
+   * @type {ol.style.Style}
+   * @private
+   */
+  this.endIconStyle_ = options.endIconStyle;
+
+
+  goog.base(this, {
+    element: element,
+    target: options.target
+  });
+
+
+  /**
+   * @type {ol.control.GoogleMapsGeocoder}
+   * @private
+   */
+  this.startGeocoder_ = new ol.control.GoogleMapsGeocoder({
+    'enableReverseGeocoding': true,
+    'target': startGeocoderElement,
+    'geocoderComponentRestrictions': this.geocoderComponentRestrictions_,
+    'iconStyle': this.startIconStyle_
+  });
+
+
+  /**
+   * @type {ol.control.GoogleMapsGeocoder}
+   * @private
+   */
+  this.endGeocoder_ = new ol.control.GoogleMapsGeocoder({
+    'enableReverseGeocoding': false,
+    'target': endGeocoderElement,
+    'geocoderComponentRestrictions': this.geocoderComponentRestrictions_,
+    'iconStyle': this.endIconStyle_
+  });
+
+
+  /**
+   * @private
+   * @type {google.maps.}
+   */
+  //this.directions_ = new google.maps.DirectionsService();
+
+};
+goog.inherits(ol.control.GoogleMapsDirections, ol.control.Control);
+
+
+/**
+ * @inheritDoc
+ */
+ol.control.GoogleMapsDirections.prototype.setMap = function(map) {
+  goog.base(this, 'setMap', map);
+  if (!goog.isNull(map)) {
+    map.addControl(this.startGeocoder_);
+    map.addControl(this.endGeocoder_);
+  }
+};
