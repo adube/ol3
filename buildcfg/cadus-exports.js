dc4e16b1683bfe470ff8e34c0267174ac3bc962e
Alexandre Dubé <adube@mapgears.com>
2014-03-13 15:14:28 -0400

    Add cacus build cfg files (not completed yet)

---
+++ buildcfg/cadus-exports.js
@@ -0,0 +1,18 @@
+goog.require('ol.Map');
+goog.require('ol.View2D');
+goog.require('ol.control.GoogleMapsDirections');
+goog.require('ol.interaction');
+goog.require('ol.interaction.DragPan');
+goog.require('ol.layer.Vector');
+goog.require('ol.proj');
+goog.require('ol.source.GeoJSON');
+goog.require('ol.style.Fill');
+goog.require('ol.style.Icon');
+goog.require('ol.style.Stroke');
+goog.require('ol.style.Style');
+
+goog.exportSymbol(
+    'ol.Map',
+    ol.Map);
+
+// todo: export required symbols
