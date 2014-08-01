2d94814f5d71d5c90e48f4a4eb11e8e94fb90080
Alexandre Dubé <adube@mapgears.com>
2014-08-01 10:00:39 -0400

    tests
    Conflicts:
    	test/spec/ol/source/clustersource.test.js

--- test/spec/ol/source/clustersource.test.js
+++ test/spec/ol/source/clustersource.test.js
@@ -1,14 +1,21 @@
 goog.provide('ol.test.source.ClusterSource');
 
+<<<<<<< HEAD
 goog.require('ol.source.Vector');
 
+=======
+>>>>>>> fbdc91e... tests
 describe('ol.source.Cluster', function() {
 
   describe('constructor', function() {
     it('returns a cluster source', function() {
       var source = new ol.source.Cluster({
+<<<<<<< HEAD
         projection: ol.proj.get('EPSG:4326'),
         source: new ol.source.Vector()
+=======
+        projection: ol.proj.get('EPSG:4326')
+>>>>>>> fbdc91e... tests
       });
       expect(source).to.be.a(ol.source.Source);
       expect(source).to.be.a(ol.source.Cluster);
